d9ca9b84c7aac93237d585fea689e04ed76b5b7d
Katematestate <80445606+Katematestate@users.noreply.github.com>
2023-02-23 13:40:02 +1300

    initial set up

--- NewsApp/src/App.vue
+++ NewsApp/src/App.vue
@@ -1,22 +1,20 @@
 <script setup>
-import NavBar from './components/home_components/NavBar.vue';
-import SearchBar from './components/home_components/Search.vue'
+import NavBar from './components/NavBar.vue';
+import SearchBar from './components/Search.vue';
 </script>
 <template>
-  <NavBar/>
-  <SearchBar/>
+  <NavBar />
+  <SearchBar />
 </template>
-<style scoped>
-</style>
+<style scoped></style>
 <script>
 export default {
-  data(){ // return part of data() is where we declare variables
-    return{ // declare variables below and separate them with comma ,
+  data() { // return part of data() is where we declare variables
+    return { // declare variables below and separate them with comma ,
     } // end of variable declaration area
   }, // End of data()
-  methods:{ // this is where we define functions that can be used in created() or template items with @click
+  methods: { // this is where we define functions that can be used in created() or template items with @click
   }, // End of methods
-  created(){ // this is where we write the JS code lines that will run once at the beginning for this component
+  created() { // this is where we write the JS code lines that will run once at the beginning for this component
   } // End of created()
 } // End of export default
-</script>
